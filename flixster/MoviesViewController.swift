89f3d4a2e36a3c635d9c30ad3e68a3b866eb12fb
Henry Rivera <henry.rivera@nyu.edu>
2020-09-24 23:19:11 -0400

    adding movie posters in app

--- flixster/MoviesViewController.swift
+++ flixster/MoviesViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
@@ -63,6 +64,11 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         // cell.textLabel!.text = title
         cell.titleLabel.text = title
         cell.synopsisLabel.text = synopsis
+        
+        let baseURL = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterURL = URL(string: baseURL + posterPath)
+        cell.posterView.af.setImage(withURL: posterURL!)
         return cell
     }
 
